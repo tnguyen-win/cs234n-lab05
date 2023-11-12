b7c71be2fe8ae7ec6898f89db90f71738d42f00b
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2023-11-12 11:25:04 -0800

    Misc

---
+++ MMABooksRESTAPI/Program.cs
@@ -0,0 +1,41 @@
+using MMABooksEFClasses.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Add CORS policy - in a production app lock this down!
+builder.Services.AddCors(options => {
+    options.AddDefaultPolicy(
+        builder => {
+            builder
+            .AllowAnyOrigin()
+            .WithMethods("post", "put", "delete", "get", "options")
+            .AllowAnyHeader();
+        });
+});
+// Adding the dbContext to the service
+builder.Services.AddDbContext<MMABooksContext>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseHttpsRedirection();
+
+// Enables the CORS policy
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
